80dc3bfaf6a47ca11e9cc0c3d7bf442d4134e698
Michal Tynior <mtynior@gmail.com>
2016-06-30 12:38:14 +0200

    Initial commit

---
+++ Starbird/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Starbird
+//
+//  Created by Michal Tynior on 30/06/2016.
+//  Copyright © 2016 Michal Tynior. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
