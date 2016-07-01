de040658162d17a8565cab34c5668f13fcf2dd63
Michal Tynior <mtynior@gmail.com>
2016-07-01 17:07:47 +0200

    Update task names

--- Source/main.swift
+++ Source/main.swift
@@ -24,30 +24,30 @@ starbird.addTask(named: "task2") { _ in
 
 starbird.startTask(named: "task1")
 
-starbird.startTask(named: "task2", beforeExecute: { _ in
-    print("Task 2 haven't started yet")
+starbird.startTask(named: "task1", beforeExecute: { _ in
+    print("Task 1 haven't started yet")
 })
 
 starbird.startTask(named: "task1", beforeExecute: ["task2"] )
 
-starbird.startTask(named: "task2", afterExecute: { _ in
-    print("Task 2  completed")
+starbird.startTask(named: "task1", afterExecute: { _ in
+    print("Task 1 completed")
 })
 
 starbird.startTask(named: "task1", afterExecute: ["test2"])
 
-starbird.startTask(named: "task2", beforeExecute: { _ in
-    
+starbird.startTask(named: "task1", beforeExecute: { _ in
+    print("Task 1 haven't started yet")
 }, afterExecute: { _ in
-    
+    print("Task 1 completed")
 })
 
 starbird.startTask(named: "task1", beforeExecute:  ["task2"], afterExecute: ["task2"])
 
 starbird.startTask(named: "task1", beforeExecute: { _ in
-    
+    print("Task 1 haven't started yet")
 }, afterExecute: ["task2"])
 
 starbird.startTask(named: "task1", beforeExecute:  ["task2"], afterExecute: { _ in
-        
+    print("Task 1 completed")
 })
